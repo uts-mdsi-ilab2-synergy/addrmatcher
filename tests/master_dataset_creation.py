69d17f80452a787be11df02a7204eec64a732ed4
isampe <isampe@gmail.com>
2021-08-28 10:21:18 +1000

    split the loading and merging process to avoid memory allocation issue

--- tests/master_dataset_creation.py
+++ tests/master_dataset_creation.py
@@ -1,164 +1,112 @@
 import pandas as pd
 
 states = ["ACT", "NSW", "NT", "OT", "QLD", "SA", "TAS", "VIC", "WA"]
-gnaf_address_detail = pd.DataFrame()
+
+au_ssc = pd.read_csv("SSC_2016_AUST.csv", dtype="str")
+
+#i = 0
+
 for state in states:
-    gnaf_address_detail_state = pd.read_csv(
+    print("--"+state+"--")
+    #address detail
+    gnaf_address_detail = pd.read_csv(
         state + "_ADDRESS_DETAIL_psv.psv", delimiter="|", dtype="str"
     )
     if state != "OT":
-        gnaf_address_detail_state["STATE"] = state
+        gnaf_address_detail["STATE"] = state
     else:
-        gnaf_address_detail_state["STATE"] = ""
-
-    if gnaf_address_detail.shape[0] == 0:
-        gnaf_address_detail = gnaf_address_detail_state
-    else:
-        gnaf_address_detail = gnaf_address_detail.append(
-            gnaf_address_detail_state, ignore_index=True
-        )
-
-gnaf_address_geocode = pd.DataFrame()
-for state in states:
-    gnaf_address_geocode_state = pd.read_csv(
+        gnaf_address_detail["STATE"] = ""
+    
+    #geocode
+    gnaf_address_geocode = pd.read_csv(
         state + "_ADDRESS_DEFAULT_GEOCODE_psv.psv", delimiter="|", dtype="str"
     )
-    if gnaf_address_geocode.shape[0] == 0:
-        gnaf_address_geocode = gnaf_address_geocode_state
-    else:
-        gnaf_address_geocode = gnaf_address_geocode.append(
-            gnaf_address_geocode_state, ignore_index=True
-        )
-
-gnaf_street_locality = pd.DataFrame()
-for state in states:
-    gnaf_street_locality_state = pd.read_csv(
+    
+    #street locality
+    gnaf_street_locality = pd.read_csv(
         state + "_STREET_LOCALITY_psv.psv", delimiter="|", dtype="str"
     )
-    if gnaf_street_locality.shape[0] == 0:
-        gnaf_street_locality = gnaf_street_locality_state
-    else:
-        gnaf_street_locality = gnaf_street_locality.append(
-            gnaf_street_locality_state, ignore_index=True
-        )
-
-gnaf_locality = pd.DataFrame()
-for state in states:
-    gnaf_locality_state = pd.read_csv(
+    
+    #locality
+    gnaf_locality = pd.read_csv(
         state + "_LOCALITY_psv.psv", delimiter="|", dtype="str"
     )
-    if gnaf_locality.shape[0] == 0:
-        gnaf_locality = gnaf_locality_state
-    else:
-        gnaf_locality = gnaf_locality.append(gnaf_locality_state, ignore_index=True)
-
-gnaf_adress_mb = pd.DataFrame()
-for state in states:
-    gnaf_adress_mb_state = pd.read_csv(
+    
+    #mesh block
+    gnaf_adress_mb = pd.read_csv(
         state + "_ADDRESS_MESH_BLOCK_2016_psv.psv", delimiter="|", dtype="str"
     )
-    if gnaf_adress_mb.shape[0] == 0:
-        gnaf_adress_mb = gnaf_adress_mb_state
-    else:
-        gnaf_adress_mb = gnaf_adress_mb.append(gnaf_adress_mb_state, ignore_index=True)
-
-
-# merge all the datasets into a single dataframe
-gnaf_address_combined = (
-    gnaf_address_detail.merge(
-        gnaf_street_locality[
-            ["STREET_LOCALITY_PID", "STREET_NAME", "STREET_TYPE_CODE"]
-        ],
-        how="inner",
-        on="STREET_LOCALITY_PID",
+    
+    gnaf_adress_mb["MB_2016"] = gnaf_adress_mb["MB_2016_PID"].apply(lambda x: x[4:])
+    
+    # merge all the datasets into a single dataframe
+    gnaf_address_combined = (
+        gnaf_address_detail.merge(
+            gnaf_street_locality[
+                ["STREET_LOCALITY_PID", "STREET_NAME", "STREET_TYPE_CODE"]
+            ],
+            how="inner",
+            on="STREET_LOCALITY_PID",
+        )
+        .merge(
+            gnaf_locality[["LOCALITY_PID", "LOCALITY_NAME"]], how="inner", on="LOCALITY_PID"
+        )
+        .merge(
+            gnaf_adress_mb[["ADDRESS_DETAIL_PID", "MB_2016"]],
+            how="inner",
+            on="ADDRESS_DETAIL_PID",
+        )
+        .merge(
+            gnaf_address_geocode[["ADDRESS_DETAIL_PID", "LONGITUDE", "LATITUDE"]],
+            how="inner",
+            on="ADDRESS_DETAIL_PID",
+        )
     )
-    .merge(
-        gnaf_locality[["LOCALITY_PID", "LOCALITY_NAME"]], how="inner", on="LOCALITY_PID"
-    )
-    .merge(
-        gnaf_adress_mb[["ADDRESS_DETAIL_PID", "MB_2016_PID"]],
-        how="inner",
-        on="ADDRESS_DETAIL_PID",
-    )
-    .merge(
-        gnaf_address_geocode[["ADDRESS_DETAIL_PID", "LONGITUDE", "LATITUDE"]],
-        how="left",
-        on="ADDRESS_DETAIL_PID",
-    )
-)
+    
+    gnaf_address_combined["LONGITUDE"] = gnaf_address_combined["LONGITUDE"].astype(float)
+    gnaf_address_combined["LATITUDE"] = gnaf_address_combined["LATITUDE"].astype(float)
 
-# replace NaN/None with emptry string, for string concatenation purposes
-gnaf_address_combined.fillna("", inplace=True)
+    gnaf_address_combined["FULL_ADDRESS"] = gnaf_address_combined[['LOT_NUMBER_PREFIX','LOT_NUMBER','LOT_NUMBER_SUFFIX',
+                                                              'FLAT_TYPE_CODE','FLAT_NUMBER_PREFIX','FLAT_NUMBER',
+                                                              'FLAT_NUMBER_SUFFIX','LEVEL_TYPE_CODE','LEVEL_NUMBER_PREFIX',
+                                                              'LEVEL_NUMBER','LEVEL_NUMBER_SUFFIX','NUMBER_FIRST_PREFIX',
+                                                              'NUMBER_FIRST','NUMBER_FIRST_SUFFIX','NUMBER_LAST_PREFIX',
+                                                              'NUMBER_LAST','NUMBER_LAST_SUFFIX','STREET_NAME',
+                                                              'STREET_TYPE_CODE','LOCALITY_NAME','STATE','POSTCODE']].fillna('').agg(''.join, axis = 1)
+    
+    
+    au_lga = pd.read_csv("LGA_2016_" + state + ".csv", dtype="str")
+    
+    mesb_block = pd.read_csv("MB_2016_" + state + ".csv", dtype="str")
+    
+    asgs = mesb_block.merge(
+            au_lga[["MB_CODE_2016", "LGA_CODE_2016", "LGA_NAME_2016"]],
+                how="inner",
+                on="MB_CODE_2016",
+            ).merge(
+                au_ssc[["MB_CODE_2016", "SSC_CODE_2016", "SSC_NAME_2016"]],
+                how="inner",
+                on="MB_CODE_2016",
+            )
 
-gnaf_address_combined["LONGITUDE"] = gnaf_address_combined["LONGITUDE"].astype(float)
-gnaf_address_combined["LATITUDE"] = gnaf_address_combined["LATITUDE"].astype(float)
-
-gnaf_address_combined["FULL_ADDRESS"] = (
-    gnaf_address_combined["LOT_NUMBER_PREFIX"].astype(str)
-    + gnaf_address_combined["LOT_NUMBER"].astype(str)
-    + gnaf_address_combined["LOT_NUMBER_SUFFIX"].astype(str)
-    + gnaf_address_combined["FLAT_TYPE_CODE"].astype(str)
-    + gnaf_address_combined["FLAT_NUMBER_PREFIX"].astype(str)
-    + gnaf_address_combined["FLAT_NUMBER"].astype(str)
-    + gnaf_address_combined["FLAT_NUMBER_SUFFIX"].astype(str)
-    + gnaf_address_combined["LEVEL_TYPE_CODE"].astype(str)
-    + gnaf_address_combined["LEVEL_NUMBER_PREFIX"].astype(str)
-    + gnaf_address_combined["LEVEL_NUMBER"].astype(str)
-    + gnaf_address_combined["LEVEL_NUMBER_SUFFIX"].astype(str)
-    + gnaf_address_combined["NUMBER_FIRST_PREFIX"].astype(str)
-    + gnaf_address_combined["NUMBER_FIRST"].astype(str)
-    + gnaf_address_combined["NUMBER_FIRST_SUFFIX"].astype(str)
-    + gnaf_address_combined["NUMBER_LAST_PREFIX"].astype(str)
-    + gnaf_address_combined["NUMBER_LAST"].astype(str)
-    + gnaf_address_combined["NUMBER_LAST_SUFFIX"].astype(str)
-    + " "
-    + gnaf_address_combined["STREET_NAME"].astype(str)
-    + " "
-    + gnaf_address_combined["STREET_TYPE_CODE"].astype(str)
-    + ", "
-    + gnaf_address_combined["LOCALITY_NAME"].astype(str)
-    + ", "
-    + gnaf_address_combined["STATE"].astype(str)
-    + " "
-    + gnaf_address_combined["POSTCODE"].astype(str)
-)
-
-
-# https://www.abs.gov.au/AUSSTATS/abs@.nsf/DetailsPage/1270.0.55.003July%202016?OpenDocument “xxxx Local Government Area ASGS Edition 2016 in .csv Format"
-au_lga = pd.DataFrame()
-for state in states:
-    au_lga_state = pd.read_csv("LGA_2016_" + state + ".csv", dtype="str")
-    if au_lga.shape[0] == 0:
-        au_lga = au_lga_state
-    else:
-        au_lga = au_lga.append(au_lga_state, ignore_index=True)
-
-
-# statistical area (ASGS 2016: https://www.abs.gov.au/AUSSTATS/abs@.nsf/DetailsPage/1270.0.55.001July%202016?OpenDocument)
-mesb_block = pd.DataFrame()
-for state in states:
-    mesb_block_state = pd.read_csv("MB_2016_" + state + ".csv", dtype="str")
-    if mesb_block.shape[0] == 0:
-        mesb_block = mesb_block_state
-    else:
-        mesb_block = mesb_block.append(mesb_block_state, ignore_index=True)
-
-au_ssc = pd.read_csv("SSC_2016_AUST.csv", dtype="str")
-au_lga = pd.read_csv("LGA_2016_AUST.csv", dtype="str")
-
-asgs = mesb_block.merge(
-    au_lga[["MB_CODE_2016", "LGA_CODE_2016", "LGA_NAME_2016"]],
-    how="inner",
-    on="MB_CODE_2016",
-).merge(
-    au_ssc[["MB_CODE_2016", "SSC_CODE_2016", "SSC_NAME_2016"]],
-    how="inner",
-    on="MB_CODE_2016",
-)
-
-asgs.to_csv("AU-ASGS.csv")
+    gnaf_address_combined = gnaf_address_combined\
+        .merge(asgs[["LGA_NAME_2016","SSC_NAME_2016",
+                     "SA4_NAME_2016","SA3_NAME_2016",
+                     "SA2_NAME_2016","SA1_7DIGITCODE_2016",
+                     "MB_CODE_2016"]], 
+        how="inner", 
+        left_on="MB_2016", 
+        right_on="MB_CODE_2016")
 
 # save the combined dataset into a single file
-gnaf_address_combined.to_csv("AU-GNAF.csv")
+
+    gnaf_address_combined.to_csv(state+"-GNAF.csv", index=False)
+    gnaf_address_combined.to_parquet(state+"-GNAF.parquet", engine='fastparquet')
+    #if i == 0:
+    #    gnaf_address_combined.to_csv("AU-GNAF.csv", index=False)
+    #else:
+    #    gnaf_address_combined.to_csv("AU-GNAF.csv", mode='a', header=False, index=False)
+        
+    #i = i + 1
 # gnaf_address_combined.to_feather('AU-GNAF.feather')
 # gnaf_address_combined.to_parquet('AU-GNAF.parquet')
