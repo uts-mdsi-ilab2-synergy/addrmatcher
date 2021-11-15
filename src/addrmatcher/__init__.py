6da81f42c24fe7eec7304dde9a86f66ea1286f09
isampe <isampe@gmail.com>
2021-11-15 17:06:52 +1100

    add regions subpackage - hierarchies

--- src/addrmatcher/__init__.py
+++ src/addrmatcher/__init__.py
@@ -1,6 +1,6 @@
 from .matcher import GeoMatcher
-from .AU import AUS
 from .region import Region
 from .hierarchy import GeoHierarchy
 from .resource import download
 from .analysis import Analysis
+from .hierarchies.AUS import AUS
